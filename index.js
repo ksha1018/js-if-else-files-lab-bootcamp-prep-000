866f5ac986e757fcae2abeeaf5f2f60c2fdbe671
ksha1018@naver.com <ksha1018@naver.com>
2018-02-25 11:03:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,3 @@ var height = 70
 module.exports = { name, height
 }
 
-name = "Susan";
-
-if(name === "Susan") {
-  console.log("The name is correct")
-} else {
-  console.log("Expected the name to be Susan")
-}
